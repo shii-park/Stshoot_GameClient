33117bf7eb135a2b426342c24dbd7813dd9e5d5d
woods-one <0820ricer@gmail.com>
2025-09-15 23:25:16 +0900

    追加 : MainGameManagerの大元を追加

---
+++ Assets/MyAssets/InGame/Scripts/GameManagers/MainGameManager.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using R3;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace StShoot.InGame.GameManagers
+{
+    public class MainGameManager : MonoBehaviour
+    {
+        public static MainGameManager Instance { get; private set; }
+        
+        private GameStateReactiveProperty _currentState = new GameStateReactiveProperty(GameState.Init);
+
+        public ReadOnlyReactiveProperty<GameState> CurrentGameState => _currentState;
+        
+        [SerializeField]
+        private TimeManager _timeManager;
+        
+        private int _currentStageIndex = 0;
+        /// <summary>
+        /// 現在のステージのインデックス
+        /// </summary>
+        public int CurrentStageIndex => _currentStageIndex;
+        
+        void Awake()
+        {
+            _currentStageIndex = 0;
+            _currentState.Subscribe(state =>
+            {
+                OnStateChanged(state);
+            });
+        }
+        
+        void OnEnable()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        public void SetGameState(GameState nextState)
+        {
+            _currentState.Value = nextState;
+        }
+        
+        void OnStateChanged(GameState nextState)
+        {
+            switch (nextState)
+            {
+                case GameState.Init:
+                    StartCoroutine(InitCoroutine());
+                    break;
+                case GameState.Ready:
+                    Ready();
+                    break;
+                case GameState.Game:
+                    Game();
+                    break;
+                case GameState.Adventure:
+                    Adventure();
+                    break;
+                case GameState.Result:
+                    Result();
+                    break;
+                default:
+                    break;
+            }
+        }
+        IEnumerator InitCoroutine()
+        {
+            yield return null;
+            
+            _currentState.Value = GameState.Ready;
+        }
+        void Ready()
+        {
+            _timeManager.StartGameReadyCountDown();
+            _currentState.Value = GameState.Game;
+        }
+
+        void Game()
+        {
+            // ゲームパートの処理
+        }
+        void Adventure()
+        {
+            // アドベンチャーパートの処理
+        }
+
+        void Result()
+        {
+            // リザルトの処理    
+        }
+    }
+}
