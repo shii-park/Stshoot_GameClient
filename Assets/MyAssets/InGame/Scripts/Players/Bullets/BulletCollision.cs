759ba2c16f64febf64503d62dd94f1478263622b
woods-one <0820ricer@gmail.com>
2025-09-15 19:36:05 +0900

    追加 : Enemyにダメージを与えられるように

--- Assets/MyAssets/InGame/Scripts/Players/Bullets/BulletCollision.cs
+++ Assets/MyAssets/InGame/Scripts/Players/Bullets/BulletCollision.cs
@@ -1,3 +1,5 @@
+using StShoot.InGame.Scripts.Common.Interfaces;
+using StShoot.InGame.Scripts.Enemys.Interfaces;
 using StShoot.InGame.Scripts.Walls.Interfaces;
 using UnityEngine;
 
@@ -11,10 +13,16 @@ namespace StShoot.InGame.Scripts.Players.Bullets
         private BulletModel _model => _presenter.Model;
         
         
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.gameObject.GetComponent<IWall>() != null)
+            var collisionObject = collision.gameObject;
+            if (collisionObject.GetComponent<IWall>() != null)
             {
+                _model.SetAvailabl(true);
+            }
+            else if (collisionObject.GetComponent<IEnemy>() != null )
+            {
+                collisionObject.GetComponent<IDamageable>()?.TakeDamage(_model.BulletPower);
                 _model.SetAvailabl(true);
             }
         }
