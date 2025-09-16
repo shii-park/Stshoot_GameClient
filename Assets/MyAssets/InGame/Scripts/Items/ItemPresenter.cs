0329ac9394b99ecaacc4209a1c9d1efe7287983b
woods-one <0820ricer@gmail.com>
2025-09-17 06:49:32 +0900

    追加 : ItemPresenterの追加

--- Assets/MyAssets/InGame/Scripts/Items/ItemPresenter.cs
+++ Assets/MyAssets/InGame/Scripts/Items/ItemPresenter.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using R3;
 
 namespace StShoot.InGame.Items
 {
     public class ItemPresenter : MonoBehaviour
     {
-        
+        [SerializeField] private ItemView _view;
+        [SerializeField] private BaseItem _model;
+        public BaseItem Model => _model;
+
+        private void Start()
+        {
+            _model.IsAvailable.Subscribe(isAlive =>
+            {
+                //_view.SetActive(isAlive);
+            });
+        }
     }
 }
