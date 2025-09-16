d3fda3591989d728be94c695dd5f2f90490e4c79
woods-one <0820ricer@gmail.com>
2025-09-17 03:30:53 +0900

    追加 : WayPointにコンストラクタを追加

--- Assets/MyAssets/InGame/Scripts/Enemies/Waypoint.cs
+++ Assets/MyAssets/InGame/Scripts/Enemies/Waypoint.cs
@@ -15,5 +15,12 @@ namespace StShoot.InGame.Enemies
         public Vector3 Position;
         public float Duration;
         public MoveType MoveType;
+        
+        public Waypoint(Vector3 position, float duration, MoveType moveType)
+        {
+            Position = position;
+            Duration = duration;
+            MoveType = moveType;
+        }
     }
 }
