542e1cbcabcd3c27f83f2c4c580281200732237b
woods-one <0820ricer@gmail.com>
2025-09-18 01:50:26 +0900

    追加 : OutGameのUI周り

---
+++ Assets/MyAssets/OutGame/Scripts/UIs/OutGameUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StShoot.OutGame.UIs
+{
+    public class OutGameUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _titleSecondHalfText;
+        [SerializeField]
+        private Text _titleFirstHalfText;
+        [SerializeField]
+        private Text _startButtonText;
+        
+    }
+}
