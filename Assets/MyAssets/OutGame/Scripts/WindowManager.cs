11f3a1b53e490fc7e2b378338b1fce77300db090
woods-one <0820ricer@gmail.com>
2025-09-23 04:46:21 +0900

    追加 : おなじ画面サイズで表示されるように

---
+++ Assets/MyAssets/OutGame/Scripts/WindowManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace StShoot.OutGame
+{
+    public class WindowManager : MonoBehaviour
+    {
+        void Start()
+        {
+            Screen.SetResolution(2000, 1500, true);
+        }
+    }
+}
